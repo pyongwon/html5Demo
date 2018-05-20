fdf8a4c35fb54e2515a76b0240ae4bc23dc60c42
pyongwonlee <pyongwonlee@hotmail.com>
2018-05-20 19:25:21 -0400

    Browser Support 1

---
+++ Html5Demo/Controllers/Html5Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Html5Demo.Controllers
+{
+    public class Html5Controller : Controller
+    {
+        // GET: Html5
+        public ActionResult BrowserSupport()
+        {
+            return View();
+        }
+    }
+}
