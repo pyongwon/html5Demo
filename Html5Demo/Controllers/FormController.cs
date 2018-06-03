eee2ec2f7d58e39f96d38396cd1eee7e3da3f7e4
pyongwonlee <pyongwonlee@hotmail.com>
2018-06-03 16:01:29 -0400

    Html5 Form

---
+++ Html5Demo/Controllers/FormController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Html5Demo.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form/FormControls
+        public ActionResult FormControls()
+        {
+            return View();
+        }
+
+        // GET: Form/Validation4
+        public ActionResult Validation4()
+        {
+            return View();
+        }
+
+        // GET: Form/Validation5
+        public ActionResult Validation5()
+        {
+            return View();
+        }
+    }
+}
